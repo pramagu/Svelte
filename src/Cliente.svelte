2c46eaac3a9a485df2e14e4a8e6eafb0fe4c10a2
pramagu <93121628+pramagu@users.noreply.github.com>
2023-03-02 21:22:52 +0100

    Update Cliente.svelte

--- src/Cliente.svelte
+++ src/Cliente.svelte
@@ -1,8 +1,9 @@
 <script>
     export let cliente = {nombre: "", apellido: ""};
 </script>
-
+<div class="px-5">
 <input id="nombre" type="text" placeholder="Nombre" bind:value={cliente.nombre}/><br />
 <input id="precio" type="text" placeholder="Apellido" bind:value={cliente.apellido}/>
+</div>
 
 <slot />
