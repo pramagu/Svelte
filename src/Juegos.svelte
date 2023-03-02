cb857f3fee98eea14dea57b055a6b789d7b03079
pramagu <93121628+pramagu@users.noreply.github.com>
2023-03-02 12:41:40 +0100

    Add files via upload

--- src/Juegos.svelte
+++ src/Juegos.svelte
@@ -33,7 +33,7 @@
 
 <div class="row">
     {#each datos as juego}
-        <div class="col-md-4 mx-2">
+        <div class="col-xs-4">
             <Juego bind:juego>
                 <Boton tipo="modificar" documento={juego} coleccion="juegos"
                     >Modificar</Boton>
