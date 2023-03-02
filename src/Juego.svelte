9597eb63a7b084a507ff3c43ce9276a7cad088a0
pramagu <93121628+pramagu@users.noreply.github.com>
2023-03-02 21:31:58 +0100

    Update Juego.svelte

--- src/Juego.svelte
+++ src/Juego.svelte
@@ -2,7 +2,9 @@
     export let juego = {nombre: "", precio: 0};
 </script>
 
+<div class="px-5">
 <input id="nombre" type="text" placeholder="Nombre" bind:value={juego.nombre}/><br />
 <input id="precio" type="number" placeholder="Precio" bind:value={juego.precio}/>
+</div>
 
 <slot />
