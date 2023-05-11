ee99824690544bc94128b6f065e63c4cc0e26445
Manuel Pablo Ramos Aguilar <93121628+pramagu@users.noreply.github.com>
2023-05-11 10:59:47 +0200

    Create githubicon.js

---
+++ githubicon.js
@@ -0,0 +1,12 @@
+
+function botar() {
+  document
+    .getElementById('#github')
+    ?.setAttribute('class', 'fa-brands fa-github fa-bounce');
+}
+
+function Nobotar() {
+  document
+    .getElementById('#github')
+    ?.setAttribute('class', 'fa-brands fa-github');
+}
